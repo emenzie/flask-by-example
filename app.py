84e2b35a80a5dcfd68d6b6d48781a58565b7f6fa
Eric <eric@ericmenzie.com>
2019-09-04 14:09:15 -0400

    mod config

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 from flask import Flask
 
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+print(os.environ['APP_SETTINGS'])
+
 @app.route('/')
 def hello():
-    return "<h1>Hello World! Eric, you did it!</h1>"
+    return "Hello World!"
+
 
 @app.route('/<name>')
 def hello_name(name):
