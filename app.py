6024f23810b72a4193112a483add89bf9a62178c
Eric <eric@ericmenzie.com>
2019-09-04 11:17:41 -0400

    staging test

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "<h1>Hello World! Eric, you did it!</h1>"
 
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if __name__ == '__main__':
     app.run()
