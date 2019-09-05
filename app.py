610ff9576dfa80bc53863ffd80963f03b677d374
Eric <eric@ericmenzie.com>
2019-09-04 20:27:41 -0400

    db stuff

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-print(os.environ['APP_SETTINGS'])
+from models import Result
+
 
 @app.route('/')
 def hello():
@@ -15,6 +19,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
