c474488bb4ecc26e5ea142cc910796c48d28b1a9
Eric <eric@ericmenzie.com>
2019-09-05 16:37:51 -0400

    added wordcount ui and get/post

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import requests
+import operator
+import re
+import nltk
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 from models import Result
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, results=results)
 
 if __name__ == '__main__':
     app.run()
